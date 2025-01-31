8040378a72b5d9740d176f023eede876a7b67739
joeloskarsson <joel.oskarsson@liu.se>
2025-01-31 21:27:04 +0100

    Fixes for extra dims in derived variables

--- mllam_data_prep/create_dataset.py
+++ mllam_data_prep/create_dataset.py
@@ -166,8 +166,6 @@ def create_dataset(config: Config):
         # Initialize the output dataset and add dimensions
         ds = xr.Dataset()
         ds.attrs.update(ds_input.attrs)
-        for dim in ds_input.dims:
-            ds = ds.assign_coords({dim: ds_input.coords[dim]})
 
         if selected_variables:
             logger.info(f"Extracting selected variables from dataset {dataset_name}")
@@ -193,6 +191,7 @@ def create_dataset(config: Config):
                     ds=ds_input,
                     derived_variable=derived_variable,
                     chunking=chunking_config,
+                    target_dims=expected_input_var_dims
                 )
 
         _check_dataset_attributes(
